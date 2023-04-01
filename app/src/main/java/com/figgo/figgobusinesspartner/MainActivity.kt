d4650af360e02215abab152be78d45048cf4cfa7
Abhishek <abhishek.figgo@gmail.com>
2023-04-01 18:32:50 +0530

    fist commit

---
+++ app/src/main/java/com/figgo/figgobusinesspartner/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.figgo.figgobusinesspartner
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.widget.ProgressBar
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+
+        progressBar.visibility = View.VISIBLE
+
+        if (isInternetAvailable(this)) {
+            val webView = findViewById<WebView>(R.id.webview)
+            webView.settings.javaScriptEnabled = true
+            webView.loadUrl("https://business-partner.figgo.in/")
+            progressBar.visibility = View.GONE
+        }else{
+            Toast.makeText(this, "Please check your Internet connectivity.. ", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun isInternetAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            return networkInfo.isConnected
+        }
+    }
+
+}
